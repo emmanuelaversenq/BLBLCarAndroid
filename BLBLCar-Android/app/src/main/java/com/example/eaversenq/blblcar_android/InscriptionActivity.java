41f317fc0d8ade8f3d7de396ba1ab606a5711b1c
JFBire <jean-francois.bire@berger-levrault.fr>
2016-02-17 09:45:39 +0100

    JF renommage des id pour les editText dans activity_inscription.xml + gestion des erreurs de saisie dans inscriptionActivity.java

--- BLBLCar-Android/app/src/main/java/com/example/eaversenq/blblcar_android/InscriptionActivity.java
+++ BLBLCar-Android/app/src/main/java/com/example/eaversenq/blblcar_android/InscriptionActivity.java
@@ -5,7 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.eaversenq.blblcar_android.model.User;
 
 public class InscriptionActivity extends Activity {
 
@@ -36,17 +40,84 @@ public class InscriptionActivity extends Activity {
     }
 
     public void onClick(View v) {
+        boolean errInscription=false;
+        boolean conducteurSelectionne=false;
+        boolean passagerSelectionne=false;
+        User user;
 
-        TextView login = (TextView) findViewById(R.id.lg);
-        TextView motpasse = (TextView) findViewById(R.id.mp);
+        TextView login = (TextView) findViewById(R.id.identification);
+        TextView motpasse = (TextView) findViewById(R.id.motDePasse);
+        TextView confirmation = (TextView) findViewById(R.id.confirmation);
+        TextView nom = (TextView) findViewById(R.id.nom);
+        TextView prenom = (TextView) findViewById(R.id.prenom);
+        TextView email = (TextView) findViewById(R.id.email);
+        TextView adresse = (TextView) findViewById(R.id.adresse);
+        TextView codePostal = (TextView) findViewById(R.id.codePostal);
+        TextView ville = (TextView) findViewById(R.id.ville);
+        CheckBox passager = (CheckBox) findViewById(R.id.passager);
+        CheckBox conducteur = (CheckBox) findViewById(R.id.conducteur);
 
-    /*    if (login.getText().toString().equals("khalil") && motpasse.getText().toString().equals("1234")) {
+        if ((login.getText().length()==0) || (login.getText().equals(null))) {
+            Toast.makeText(this, "Le Login doit être renseigné", Toast.LENGTH_LONG).show();
+            errInscription=true;
+        }
+        if ((motpasse.getText().length()==0) || (motpasse.getText().equals(null))) {
+            Toast.makeText(this, "Le Mot de Passe doit être renseigné", Toast.LENGTH_LONG).show();
+            errInscription=true;
+        }
+        if ((confirmation.getText().length()==0) || (confirmation.getText().equals(null))) {
+            Toast.makeText(this, "La Confirmation du Mot de Passe doit être renseignée", Toast.LENGTH_LONG).show();
+            errInscription=true;
+        }
+        if (!(confirmation.getText().toString()).equals(motpasse.getText().toString())) {
+            Toast.makeText(this, "Le mot de passe et sa confirmation doit être identique", Toast.LENGTH_LONG).show();
+        }
+        if ((nom.getText().length()==0) || (nom.getText().equals(null))) {
+            Toast.makeText(this, "Le nom doit être renseigné", Toast.LENGTH_LONG).show();
+            errInscription=true;
+        }
+        if ((prenom.getText().length()==0) || (prenom.getText().equals(null))) {
+            Toast.makeText(this, "Le Prénom doit être renseigné", Toast.LENGTH_LONG).show();
+            errInscription=true;
+        }
+        if ((email.getText().length()==0) || (email.getText().equals(null))) {
+            Toast.makeText(this, "L'e.mail doit être renseigné", Toast.LENGTH_LONG).show();
+            errInscription=true;
+        }
+        if (!(email.getText().toString().matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ))){
+            Toast.makeText(this, "Veuillez saisir une adresse mail valide", Toast.LENGTH_LONG).show();
+            errInscription=true;
+        }
+        if ((adresse.getText().length()==0) || (adresse.getText().equals(null))) {
+            Toast.makeText(this, "L'adresse doit être renseignée", Toast.LENGTH_LONG).show();
+            errInscription=true;
+        }
+        if ((codePostal.getText().length()==0) || (codePostal.getText().equals(null))) {
+            Toast.makeText(this, "Le code postal doit être renseigné", Toast.LENGTH_LONG).show();
+            errInscription=true;
+        }
+        if ((ville.getText().length()==0) || (ville.getText().equals(null))) {
+            Toast.makeText(this, "La ville doit être renseignée", Toast.LENGTH_LONG).show();
+            errInscription=true;
+        }
+        if ((!conducteur.isChecked())&&(!passager.isChecked())){
+            Toast.makeText(this, "vous devez cocher passager, conducteur ou les deux", Toast.LENGTH_LONG).show();
+            errInscription=true;
+        }else {
+            if (conducteur.isChecked()) {
+                 conducteurSelectionne=true;
+            }
+            if (passager.isChecked()) {
+                passagerSelectionne=true;
+            }
 
-            Toast.makeText(this, "mot de passe corret", Toast.LENGTH_LONG).show();
-
+        }
+        if (errInscription==true) {
+            Toast.makeText(this, "Echec de l'inscription", Toast.LENGTH_LONG).show();
         } else {
-            Toast.makeText(this, "Login ou mot de passe incorrect", Toast.LENGTH_LONG).show();
-
-        }*/
+            user=new User(login.toString(), motpasse.toString(), nom.toString(), prenom.toString(), email.toString(),
+                    adresse.toString(), codePostal.toString(), ville.toString(), passagerSelectionne, conducteurSelectionne);
+            Toast.makeText(this, "Inscription effectuée avec succès", Toast.LENGTH_LONG).show();
+        }
     }
 }
